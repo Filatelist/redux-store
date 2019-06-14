3f1283ac47fd9e376714880906ff3621c4d02362
Serhii Shovkovuy <sergshovkanser@gmail.com>
2019-06-14 12:24:03 +0300

    first commit

---
+++ precache-manifest.9996b8d49a0a0d59542fb56f16399db4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48fd3199fafe69e2c6011f84d9b37396",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d20ca62d895f989ac54",
+    "url": "/static/css/main.5332b72d.chunk.css"
+  },
+  {
+    "revision": "e3f234e30bda1cf5acef",
+    "url": "/static/js/2.d599be85.chunk.js"
+  },
+  {
+    "revision": "7d20ca62d895f989ac54",
+    "url": "/static/js/main.2c8e0879.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
